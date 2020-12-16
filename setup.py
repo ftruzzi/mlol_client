e403b852e091f6f9179310a5441fa3c72b8b0f1f
Francesco Truzzi <francesco@truzzi.me>
2020-12-16 12:14:05 +0100

    bump python version

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ftruzzi/mlol_client",
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
